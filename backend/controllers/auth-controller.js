682e5675d2bd70dce48551c6588f96abaab8c1c4
Raheem Ajibona <ajibonaraheem@gmail.com>
2026-05-08 15:14:17 +0100

    forntend-url

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -45,7 +45,7 @@ const register = async (req, res, next) => {
 
     //sending Email
 
-    const verificationLink = `https://livestock-cost-estimator.vercel.app/verify-email?token=${verificationToken}&email=${newUser.email}`;
+    const verificationLink = `${process.env.FRONTEND_URL}/verify-email?token=${verificationToken}&email=${newUser.email}`;
     await sendVerificationEmail({
       name: newUser.name,
       email: newUser.email,
